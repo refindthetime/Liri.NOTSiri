f6d3bb686f132065f299d570d510a2a51492ee9a
Derek Cassel <derekdcassel@gmail.com>
2017-08-25 10:54:46 -0600

    commiting app

---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'AebHB0UcHg0xmms0Bbakr5xVj',
+  consumer_secret: 'YcF3J7uToaAVFykZ3mTvyOCakRcN7nxyZHu2f8o1AvmCOUBfRH',
+  access_token_key: '901101904091947008-oTbvoMT8ydmXWy0C2KWOb3i94lHpIIF',
+  access_token_secret: '48oZX2K0CIeSjQUHpTSQWkDK0sPbgjjuEVnmxnoEUjxJ4',
+}
+
+
+exports.spotifyKeys = {
+  consumer_key: '0f10c3966f4049df9c559ebc4ee9bd0f',
+  consumer_secret: '1e1aa64f317f4597bfb4163e979f995d',
+  access_token_key: '',
+  access_token_secret: '',
+}
---
+++ liri.js
@@ -0,0 +1,160 @@
+//Grab data from keys.js
+var keys = require('./keys.js');
+var request = require('request');
+var twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var client = new twitter(keys.twitterKeys);
+var fs = require('fs');
+
+//Stored argument's array
+var UserInput = process.argv;
+var command = process.argv[2];
+//movie or song
+var x = "";
+//attaches multiple word arguments
+for (var i=3; i<UserInput.length; i++){
+  if(i>3 && i<UserInput.length){
+    x = x + "+" + UserInput[i];
+  } else{
+    x = x + UserInput[i];
+  }
+}
+
+//switch case
+switch(command){
+  case "my-tweets":
+    showTweets();
+  break;
+
+  case "spotify-this-song":
+    if(x){
+      spotifySong(x);
+    } else{
+      spotifySong("Fluorescent Adolescent");
+    }
+  break;
+
+  case "movie-this":
+    if(x){
+      omdbData(x)
+    } else{
+      omdbData("I Am Legend")
+    }
+  break;
+
+  case "do-what-it-says":
+    doThing();
+  break;
+
+  default:
+    console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
+  break;
+}
+
+function showTweets(){
+  //Display last 20 Tweets
+  var screenName = {screen_name: 'byRefindthetime'};
+  client.get('statuses/user_timeline', screenName, function(error, tweets, response){
+    if(!error){
+      for(var i = 0; i<tweets.length; i++){
+        var date = tweets[i].created_at;
+        console.log("byRefindthetime: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        console.log("-----------------------");
+        
+        //adds text to log.txt file
+        fs.appendFile('log.txt', "byRefindthetime: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        fs.appendFile('log.txt', "-----------------------");
+      }
+    }else{
+      console.log('Error occurred');
+    }
+  });
+}
+
+ 
+
+
+function spotifySong(song){
+  var spotify = new Spotify({
+  id:'0f10c3966f4049df9c559ebc4ee9bd0f',
+  secret: '1e1aa64f317f4597bfb4163e979f995d'
+});
+  spotify.search({ type: 'track', query: song}, function(error, data){
+    if(!error){
+      for(var i = 0; i < data.tracks.items.length; i++){
+        var songData = data.tracks.items[i];
+        //artist
+        console.log("Artist: " + songData.artists[0].name);
+        //song name
+        console.log("Song: " + songData.name);
+        //spotify preview link
+        console.log("Preview URL: " + songData.preview_url);
+        //album name
+        console.log("Album: " + songData.album.name);
+        console.log("-----------------------");
+        
+        //adds text to log.txt
+        fs.appendFile('log.txt', songData.artists[0].name);
+        fs.appendFile('log.txt', songData.name);
+        fs.appendFile('log.txt', songData.preview_url);
+        fs.appendFile('log.txt', songData.album.name);
+        fs.appendFile('log.txt', "-----------------------");
+      }
+    } else{
+      console.log('Error occurred.');
+    }
+  });
+}
+
+function omdbData(movie){
+  var omdbURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
+
+  request(omdbURL, function (error, response, body){
+    if(!error && response.statusCode == 200){
+      var body = JSON.parse(body);
+
+      console.log("Title: " + body.Title);
+      console.log("Release Year: " + body.Year);
+      console.log("IMdB Rating: " + body.imdbRating);
+      console.log("Country: " + body.Country);
+      console.log("Language: " + body.Language);
+      console.log("Plot: " + body.Plot);
+      console.log("Actors: " + body.Actors);
+      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+      //adds text to log.txt
+      fs.appendFile('log.txt', "Title: " + body.Title);
+      fs.appendFile('log.txt', "Release Year: " + body.Year);
+      fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
+      fs.appendFile('log.txt', "Country: " + body.Country);
+      fs.appendFile('log.txt', "Language: " + body.Language);
+      fs.appendFile('log.txt', "Plot: " + body.Plot);
+      fs.appendFile('log.txt', "Actors: " + body.Actors);
+      fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
+      fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
+
+    } else{
+      console.log('Error occurred.')
+    }
+    if(movie === "I Am Legend"){
+      console.log("-----------------------");
+      console.log("If you haven't watched 'I Am Legend', then you should: http://http://www.imdb.com/title/tt0480249/?ref_=fn_al_tt_1");
+      console.log("It's on Netflix(maybe)!");
+
+      //adds text to log.txt
+      fs.appendFile('log.txt', "-----------------------");
+      fs.appendFile('log.txt', "If you haven't watched 'I Am Legend', then you should: http://www.imdb.com/title/tt0485947/");
+      fs.appendFile('log.txt', "It's on Netflix(maybe)!");
+    }
+  });
+
+}
+
+function doThing(){
+  fs.readFile('random.txt', "utf8", function(error, data){
+    var txt = data.split(',');
+
+    spotifySong(txt[1]);
+  });
+}
